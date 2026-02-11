49978dab2dc174776d5287c17366cfe5adfb1430
late-parrot <thelateparrot42@gmail.com>
2026-02-10 21:13:36 -0800

    Create Title Screen

---
+++ scripts/title_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var initial_ship_pos = %Ship.position
+@onready var initial_ship_rot = %Ship.rotation
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _ready() -> void:
+	_on_ship_moved()
+	
+func _on_ship_moved() -> void:
+	await get_tree().create_timer(0.5).timeout
+	var tween = get_tree().create_tween()
+	tween.tween_property(%Ship, "position",
+		initial_ship_pos+Vector2(randf_range(-10,10), randf_range(-10,10)), 2)
+	tween.tween_property(%Ship, "rotation", initial_ship_rot+randf_range(-.1,.1), 3)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_callback(_on_ship_moved)
